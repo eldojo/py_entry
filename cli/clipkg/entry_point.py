e489589ba9e07fe824dd683d1888079842235fee
Christian López Barrón <chris.gfz@gmail.com>
2018-07-21 11:41:42 +1000

    added loglevel config example

--- cli/clipkg/entry_point.py
+++ cli/clipkg/entry_point.py
@@ -1,2 +1,17 @@
 import sys
-print(sys.argv)
+import argparse
+import logging
+
+logger = logging.getLogger()
+
+if __name__=='__main__':
+    logging.basicConfig()
+    logger.info('This is an INFO')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--foo', help='get some foo!')
+    parser.add_argument('-l', '--loglevel', help='logging level', choices=['INFO', 'DEBUG', 'WARNING'])
+    args = parser.parse_args()
+    logger.setLevel(getattr(logging, args.loglevel))
+    logger.debug('This is a DEBUG')
+    logger.warning('This is a WARNING')
+    logger.error('This is an ERROR')
