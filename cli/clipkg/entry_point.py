900de7a16b5312318154f6122148f66b32714cda
Alan Manuel Acosta Salas <alan@alans-mbp.lan>
2018-07-27 20:10:26 -0500

    passing args to a function inside the code

--- cli/clipkg/entry_point.py
+++ cli/clipkg/entry_point.py
@@ -4,14 +4,23 @@ import logging
 
 logger = logging.getLogger()
 
-if __name__=='__main__':
+def printo():
+    print(args.loglevel)
+    print(args.foo)
+
+def parse_arg():
     logging.basicConfig()
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--foo', help='get some foo!')
-    parser.add_argument('-l', '--loglevel', help='logging level', choices=['INFO', 'DEBUG', 'WARNING'])
+    parser.add_argument('-f', '--foo', default='este valor de foo es default, perro', help='Foo argument is setted to (default: %(default)s)')
+    parser.add_argument('-l', '--loglevel', default='DEBUG',help='logging level, choices are: %(choices)s', choices=['INFO', 'DEBUG', 'WARNING'])
     args = parser.parse_args()
     logger.setLevel(getattr(logging, args.loglevel))
+    return(args)
+
+if __name__=='__main__':
+    args = parse_arg()
     logger.info('This is an INFO')
     logger.debug('This is a DEBUG')
     logger.warning('This is a WARNING')
     logger.error('This is an ERROR')
+    printo()
