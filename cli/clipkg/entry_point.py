7a5fd66489833c1b8771e651bc5dc86787471f1a
Christian López Barrón <chris.gfz@gmail.com>
2018-07-28 11:38:30 +1000

    removed args from inner function

--- cli/clipkg/entry_point.py
+++ cli/clipkg/entry_point.py
@@ -4,9 +4,10 @@ import logging
 
 logger = logging.getLogger()
 
-def printo():
-    print(args.loglevel)
-    print(args.foo)
+def printo(foo, bar=None, loglevel=None):
+    for k, v in locals().items():
+        print('{} value: {}'.format(k, v))
+
 
 def parse_arg():
     logging.basicConfig()
@@ -17,10 +18,14 @@ def parse_arg():
     logger.setLevel(getattr(logging, args.loglevel))
     return(args)
 
+
 if __name__=='__main__':
+    printo('Direct call')
     args = parse_arg()
+    kwargs = vars(args)
+    logger.info(vars(args))
     logger.info('This is an INFO')
     logger.debug('This is a DEBUG')
     logger.warning('This is a WARNING')
     logger.error('This is an ERROR')
-    printo()
+    printo(**kwargs)
